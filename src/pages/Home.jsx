d8367245de518f70ba56b7e1d2fcbe9b2df010ff
NicoleGluj <nicolegluj@gmail.com>
2026-02-15 23:30:24 -0300

    fixed: Home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,92 +73,104 @@ export const Home = () => {
             </div>
           </div>
         </section >
-        <section className="bg-[#0B0B14] py-20 px-10 grid lg:grid-cols-2 gap-10  items-stretch">
-          <div className="text-white flex flex-col sm:flex-row lg:flex-col gap-4 sm:gap-10 justify-center">
-            <h2 className="text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl leading-tight">
-              Diseño estratégico con foco en el
-              <span className="font-semibold"> usuario</span>.
-            </h2>
-            <p className="text-justify text-sm sm:text-[18px] lg:text-lg 2xl:text-2xl leading-relaxed text-white/80 font-light">
-              En NOVA Studio ayudamos a marcas y emprendedores a construir una presencia digital
-              sólida, coherente y memorable. Combinamos diseño, tecnología y estrategia para crear
-              sitios web funcionales, estéticos y alineados con los objetivos de cada proyecto.
-              <br /><br />
-              Creemos en procesos simples, comunicación clara y soluciones digitales que realmente aporten valor.
-            </p>
-          </div>
-          <div className="flex items-stretch justify-center">
-            <img
-              className="sm:hidden lg:block w-full h-full object-contain -translate-x-2"
-              src={equilibrioHome}
-              alt="Equilibrio"
-            />
+
+        <section className="bg-[#0B0B14] pt-20 pb-10 sm:pb-15 px-10">
+          <div className="text-white flex flex-col sm:flex-row gap-4 sm:gap-10 justify-center items-center">
+            <div className="lg:min-w-[45%] 2xl:min-w-[40%]">
+              <h2 className="text-[45px] md:text-5xl lg:text-6xl 2xl:text-7xl leading-tight">
+                Diseño estratégico con foco en el
+                <span className="font-semibold"> usuario</span>.
+              </h2>
+            </div>
+            <div>
+              <p className="text-justify text-sm sm:text-[18px] lg:text-lg 2xl:text-2xl leading-relaxed text-white/80 font-light">
+                En NOVA Studio ayudamos a marcas y emprendedores a construir una presencia digital
+                sólida, coherente y memorable. Combinamos diseño, tecnología y estrategia para crear
+                sitios web funcionales, estéticos y alineados con los objetivos de cada proyecto.
+                <br /><br />
+                Creemos en procesos simples, comunicación clara y soluciones digitales que realmente aporten valor.
+              </p>
+            </div>
           </div>
         </section>
+
         <section >
           <RotatingCarouselCards />
         </section>
-        <section className="bg-[#0B0B14] py-10 sm:py-20  px-10 grid sm:grid-cols-2 gap-10 items-stretch">
-          <div className="flex items-stretch justify-center lg:mb-20">
-            <img
-              className="hidden sm:block  object-contain "
-              src={flechaHome}
-              alt="Equilibrio"
-            />
-          </div>
-          <div className="text-white flex flex-col gap-4 justify-center">
-            <div className="border-b">
-              <h2 className="text-3xl lg:text-4xl 2xl:text-5xl font-light mb-5 uppercase">
+
+        <section className="bg-[#0B0B14] pt-30 pb-20 px-10 ">
+          <div className="text-white">
+            <div >
+              <h2 className="text-3xl lg:text-4xl 2xl:text-5xl font-light mb-10 uppercase">
                 Nuestros pasos
               </h2>
-              <h2 className="text-lg lg:text-3xl font-medium mb-1">
-                01 - Analisis
-              </h2>
-              <p className="font-light mb-4 text-sm lg:text-xl">
+            </div>
+            <div className="flex flex-col lg:items-center sm:flex-row text-white gap-2 sm:gap-10 border-b border-white/40 pb-7">
+              <div className="flex gap-10 items-center min-w-[30%] lg:min-w-[20%]">
+                <h2 className="text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl">
+                  01
+                </h2>
+                <h3 className="text-xl lg:text-2xl 2xl:text-3xl uppercase">
+                  Analisis
+                </h3>
+              </div>
+              <p className=" text-justify text-sm lg:text-2xl 2xl:text-3xl leading-relaxed text-white/80 font-light sm:pl-10">
                 Entendemos tu marca, tus objetivos y tus usuarios para definir una estrategia clara.
               </p>
             </div>
-            <div className="border-b">
-              <h2 className="text-lg lg:text-3xl font-medium mb-1">
-                02 - Diseño
-              </h2>
-              <p className="font-light mb-4 text-sm lg:text-xl">
-                Creamos una propuesta visual alineada a tu identidad y enfocada en la experiencia.            </p>
+            <div className="flex flex-col lg:items-center sm:flex-row  text-white gap-2 sm:gap-10 border-b border-white/40 pb-7 pt-7">
+              <div className="flex gap-10 items-center min-w-[30%] lg:min-w-[20%]">
+                <h2 className="text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl">
+                  02
+                </h2>
+                <h3 className="text-xl lg:text-2xl 2xl:text-3xl uppercase">
+                  Diseño
+                </h3>
+              </div>
+              <p className=" text-justify text-sm lg:text-2xl 2xl:text-3xl leading-relaxed text-white/80 font-light sm:pl-10">
+                Creamos una propuesta visual alineada a tu identidad y enfocada en la experiencia.
+              </p>
             </div>
-            <div className="border-b">
-              <h2 className="text-lg lg:text-3xl font-medium mb-1">
-                03 - Desarrollo
-              </h2>
-              <p className="font-light mb-4 text-sm lg:text-xl">
-                Transformamos el diseño en un producto digital funcional, moderno y optimizado.            </p>
+            <div className="flex flex-col lg:items-center sm:flex-row  text-white gap-2 sm:gap-10 border-b border-white/40 pb-7 pt-7">
+              <div className="flex gap-10 items-center min-w-[30%]  lg:min-w-[20%]">
+                <h2 className="text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl">
+                  03
+                </h2>
+                <h3 className="text-xl lg:text-2xl 2xl:text-3xl uppercase">
+                  Desarrollo
+                </h3>
+              </div>
+              <p className=" text-justify text-sm lg:text-2xl 2xl:text-3xl leading-relaxed text-white/80 font-light sm:pl-10">
+                Transformamos el diseño en un producto digital funcional, moderno y optimizado.
+              </p>
             </div>
-            <div className="border-b">
-              <h2 className="text-lg lg:text-3xl font-medium mb-1">
-                04 - Entrega
-              </h2>
-              <p className="font-light mb-4 text-sm lg:text-xl">
-                Ajustamos los últimos detalles y dejamos todo listo para salir al mundo.            </p>
+            <div className="flex flex-col lg:items-center sm:flex-row  text-white gap-2 sm:gap-10 border-b border-white/40 pb-7 pt-7">
+              <div className="flex gap-10 items-center min-w-[30%] lg:min-w-[20%]">
+                <h2 className="text-4xl sm:text-5xl lg:text-6xl 2xl:text-7xl">
+                  04
+                </h2>
+                <h3 className="text-xl lg:text-2xl 2xl:text-3xl uppercase">
+                  Entrega
+                </h3>
+              </div>
+              <p className=" text-justify text-sm lg:text-2xl 2xl:text-3xl leading-relaxed text-white/80 font-light sm:pl-10">
+                Ajustamos los últimos detalles y dejamos todo listo para salir al mundo.
+              </p>
             </div>
           </div>
+
         </section>
-        <section className="bg-[#0B0B14] py-10 2xl:py-1 px-10 grid sm:grid-cols-2 gap-15 items-stretch">
+        <section className="bg-[#0B0B14] pb-30 pt-10 px-10 items-stretch">
           <div className="text-white flex flex-col gap-4 justify-center">
             <h2 className="text-4xl md:text-4xl lg:text-6xl leading-tight">
               El buen diseño no solo se ve bien.<br />Funciona, comunica y genera         <span className="font-semibold">resultados</span>.
             </h2>
             <Link
-              className="py-2 px-5 rounded-3xl border font-medium text-xl mt-4"
+              className="py-2 px-5 rounded-3xl border border-white/80 font-medium text-xl lg:text-2xl 2xl:text-3xl mt-4"
               to={"/"}>Contactanos </Link>
           </div>
-          <div className="flex items-stretch justify-center">
-            <img
-              className="w-full h-full object-contain "
-              src={metricasHome}
-              alt="Equilibrio"
-            />
-          </div>
         </section >
-      </Layout>
+      </Layout >
     </>
   )
 }
