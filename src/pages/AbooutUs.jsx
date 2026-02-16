2d4d00f3920229df96cb5c5f86dd0f313518418b
NicoleGluj <nicolegluj@gmail.com>
2026-02-15 22:45:29 -0300

    fixed: AboutUs content

--- src/pages/AbooutUs.jsx
+++ src/pages/AbooutUs.jsx
@@ -9,8 +9,8 @@ export const AboutUs = () => {
         <section className="pt-25 pb-10 px-10">
           <h2 className="text-white py-5 px-5 text-2xl border-2 w-full rounded-3xl font-regular uppercase">Quienes somos</h2>
         </section>
-        <section className="bg-[#0B0B14] py- px-10  gap-10  items-stretch">
-          <div className="text-white flex flex-col gap-4 sm:gap-10 justify-center">
+        <section className="bg-[#0B0B14] px-10 gap-6 lg:gap-20  items-stretch flex flex-col lg:flex-row">
+          <div className="text-white flex flex-col  gap-4 sm:gap-4 justify-center">
             <h2 className="text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl leading-tight">
               Diseñamos experiencias digitales con propósito.
             </h2>
@@ -18,30 +18,27 @@ export const AboutUs = () => {
               Somos un estudio creativo enfocado en diseño y desarrollo frontend, donde cada proyecto nace de una idea clara y se construye con atención al detalle.
             </p>
           </div>
-        </section>
-        <section className="mt-15">
-          <div className="flex justify-center items-center gap-10">
-            <div className="w-20 h-px bg-white"> </div>
-            <p className="text-white font-medium text-justify max-w-[250px] sm:max-w-[400px] lg:max-w-[750px]">
+          <div className="flex justify-center items-center gap-10 ">
+            <p className="border border-white/70 rounded-4xl p-6 text-white font-light text-justify h-full lg:max-w-[900px] flex justify-center items-center lg:text-lg 2xl:text-xl">
               NOVA Studio nace con la idea de crear experiencias digitales que no solo se vean bien, sino que funcionen.Creemos que el diseño es una herramienta estratégica, y que la tecnología debe estar al servicio de las personas.
             </p>
-            <div className="w-20 h-px bg-white"> </div>
           </div>
         </section>
-        <section className="flex flex-col bg-[#0B0B14] px-10 border-b  border-b-white">
+
+        <section className="flex flex-col bg-[#0B0B14] px-10 border-b border-b-white text-white">
           <h2 className="text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl leading-tight text-white mt-15 font-light italic">
             Que nos <span className="font-medium">diferencia</span>
           </h2>
-          <p className="mt-5 border border-white p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
+          <p className="mt-5 border border-white/20 p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
             Diseño centrado en el usuario
           </p>
-          <p className="mt-5 border border-white p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
+          <p className="mt-5 border border-white/20 p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
             Interfaces claras, modernas y funcionales
           </p>
-          <p className="mt-5 border border-white p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
+          <p className="mt-5 border border-white/20 p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl">
             Desarrollo frontend optimizado y escalable
           </p>
-          <p className="mt-5 border border-white p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl mb-10">
+          <p className="mt-5 border border-white/20 p-4 rounded-4xl text-white px-7 italic text-sm sm:text-[18px] lg:text-xl 2xl:text-2xl mb-10">
             Comunicación simple y procesos transparentes
           </p>
         </section>
